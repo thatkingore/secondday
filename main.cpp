7e89a784debc130244466d4bd524943db48deca5
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-03 22:47:00 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  secondday
+//
+//  Created by Ore Daodu on 03/11/2022.
+//
+
+// Start of Template
+
+// #include <iostream>
+
+// int main(int argc, const char * argv[]) {
+    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+// End of Template
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hi Hi!" << endl;
+    
+    return 0;
+}
