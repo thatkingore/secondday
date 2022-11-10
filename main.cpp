d49720d4b862ce4093a98803d347d3aa8b348f52
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-10 22:25:33 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,6 @@
 //  Created by Ore Daodu on 03/11/2022.
 //
 
-// Start of Template
-
-// #include <iostream>
-
-// int main(int argc, const char * argv[]) {
-    // insert code here...
-//    std::cout << "Hello, World!\n";
-//    return 0;
-//}
-// End of Template
-
 #include <iostream>
 #include <cmath>
 
