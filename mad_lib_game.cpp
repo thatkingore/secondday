d7925af4cf7b854c34de6847398f568e29c3e9af
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-17 20:14:22 +0000

    Add files via upload

---
+++ mad_lib_game.cpp
@@ -0,0 +1,41 @@
+//
+//  mad_lib_game.cpp
+//  secondday
+//
+//  Created by Ore Daodu on 17/11/2022.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Mad Lib Game
+
+int mad_lib_game()
+{
+    
+    string colour, pluralnoun, celeb;
+    
+    cout << "What is your favourite colour: ";
+    cin >> colour;
+    cout << "And the plural form of a word: ";
+    cin >> pluralnoun;
+    cout << "Who is your favourite person: ";
+    cin >> celeb;
+    
+    //  cout << "What is your favourite colour: ";
+    //  getline(cin, colour);
+    //  cout << "And the plural form of a word: ";
+    //  getline(cin, pluralnoun);
+    //  cout << "Who is your favourite person: ";
+    //  getline(cin, celeb);
+    
+    cout << "Roses are " << colour << endl;
+    cout << pluralnoun << " are blue" << endl;
+    cout << "I love " << celeb << endl;
+    cout << "\n";
+    
+    return 0;
+    
+}
