bfde9dfd7859821c8112980268bdefec60929757
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-17 20:04:11 +0000

    Add files via upload

---
+++ using_variables.cpp
@@ -0,0 +1,28 @@
+//
+//  using_variables.cpp
+//  secondday
+//
+//  Created by Ore Daodu on 17/11/2022.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Using Variables
+
+int using_variables()
+{
+    
+    string characterName = "John";
+    int characterAge;
+    characterAge = 35;
+    cout << "There was once a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
+    cout << "\n\n";
+    
+    return 0;
+}
