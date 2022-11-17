9dd525f0f1a0757ac377f713854ec9030dcc64d5
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-17 20:14:39 +0000

    Add files via upload

---
+++ using_arrays.cpp
@@ -0,0 +1,24 @@
+//
+//  using_arrays.cpp
+//  secondday
+//
+//  Created by Ore Daodu on 17/11/2022.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Using Arrays
+
+int using_arrays()
+{
+    int randos[20] = {4, 8, 15, 16, 23, 42};
+    cout << randos[2] << endl;
+    randos[19] = 7;
+    cout << randos[19] << endl;
+    cout << "\n";
+    
+    return 0;
+}
