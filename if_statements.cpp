1d595bfd9db3eb120ce0ad9c1b7dc82622f34f04
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-20 23:44:19 +0000

    Update if_statements.cpp

--- if_statements.cpp
+++ if_statements.cpp
@@ -10,9 +10,22 @@
 
 using namespace std;
 
+int getMax(int num1, int num2) {
+    
+    int result;
+    
+    if(num1 > num2) {
+        result = num1;
+    } else {
+        result = num2;
+    }
+    
+    return result;
+}
 
-int if_statements()
-{
+int if_statements() {
+    
+    cout << "The maximum number is " << getMax(2, 5) << endl; 
     
     bool isMale = true;
     bool isTall = false;
