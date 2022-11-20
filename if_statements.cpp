2db5253b370d623f1e5bda6a5fa0198312fb0a41
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-20 23:48:35 +0000

    Update if_statements.cpp

--- if_statements.cpp
+++ if_statements.cpp
@@ -23,9 +23,25 @@ int getMax(int num1, int num2) {
     return result;
 }
 
+int moreMax(int num1, int num2, int num3) {
+    
+    int result;
+    
+    if(num1 >= num2 && num1 >= num3) {
+        result = num1;
+    } else if(num2 >= num1 && num2 >= num3) {
+        result = num2;
+    } else {
+        result = num3;
+    }
+    
+    return result;
+}
+
 int if_statements() {
     
-    cout << "The maximum number is " << getMax(2, 5) << endl; 
+    cout << "The maximum number is " << getMax(2, 5) << endl;
+    cout << "The maximum number is " << moreMax(2, 8, 5) << endl;
     
     bool isMale = true;
     bool isTall = false;
