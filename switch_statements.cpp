e7bdf2ef5f05c921428815a245cc233b1d1a1a54
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-22 23:03:41 +0000

    Add files via upload

---
+++ switch_statements.cpp
@@ -0,0 +1,53 @@
+//
+//  switch_statements.cpp
+//  secondday
+//
+//  Created by Ore Daodu on 22/11/2022.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+string getDayOfWeek (int dayNum) {
+    string dayName;
+    
+    switch (dayNum) {
+    case 0:
+        dayName = "Sunday";
+        break;
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    default:
+        dayName = "Invalid Day Number";
+    }
+    
+    return dayName;
+}
+
+
+int switch_statement()
+{
+    cout << getDayOfWeek(10);
+    return 0;
+    
+}
+    
