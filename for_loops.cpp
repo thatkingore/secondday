f86bb15b0f5b1b8becb64ed547648062954dd17b
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-29 15:52:55 +0000

    Add files via upload

---
+++ for_loops.cpp
@@ -0,0 +1,24 @@
+//
+//  for_loops.cpp
+//  secondday
+//
+//  Created by Ore Daodu on 29/11/2022.
+//
+
+
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int for_loops()
+{
+    
+    int nums[] = {1, 2, 5, 7, 4};
+    for(int i = 0; i < 5; i++){
+        cout << nums[i] << endl;
+    }
+    
+    return 0;
+}
