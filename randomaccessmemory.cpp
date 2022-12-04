70d807584b6bea91e10792c631338d61a9cf2ecc
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-12-04 23:11:43 +0000

    Add files via upload

---
+++ randomaccessmemory.cpp
@@ -0,0 +1,22 @@
+//
+//  randomaccessmemory.cpp
+//  secondday
+//
+//  Created by Ore Daodu on 04/12/2022.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int pointers()
+{
+    int age = 23;
+    double gpa = 80;
+    string name = "Joy";
+    
+    cout << "\n\n" << endl;
+    
+    return 0;
+}
